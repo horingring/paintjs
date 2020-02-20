e7e3d08a3b5bde0fc6eb5507cf1fbbfdc61e0c1c
Horingring <dldldksl@naver.com>
2020-02-20 18:45:27 +0900

    NomadCoding0219-2

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+
+canvas.width = 500;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,25 +14,31 @@ function stopPainting(){
     painting = false;
 }
 
+function startPainting(){
+    painting = true;
+}
+
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();    //path를 항상 초기화?
+        ctx.moveTo(x, y);   //그리고나서 x,y로 옮긴다?
+    } else{
+        ctx.lineTo(x, y);   //path와 line은 다른가?
+        ctx.stroke();   //line이 없으면 stroke을 할 수 없는건가?
+     }
 }
 
-function onMouseDown(event){
-   console.log('Painting');
-   painting = true;
+function handleColorClick(event){
+    console.log(event.target.style);
 }
-
-function onMouseUp(event){
-    console.log('stopPainting');
-    stopPainting();
-}
-
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
